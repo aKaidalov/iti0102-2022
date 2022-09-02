5a137c8f7a1fd197a9d716c677995e37a1c77d11
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-02 16:48:46 +0300

    1. ulesanne. Lahendus 1

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,2 +1,5 @@
 """Print Hello."""
 name = input("What is your name? ")
+num1 = int(input(f"Hello, {name}! Enter a random number: "))
+num2 = int(input("Great! Now enter a second random number: "))
+print(f"{num1} + {num2} = " + str(num1 + num2))
