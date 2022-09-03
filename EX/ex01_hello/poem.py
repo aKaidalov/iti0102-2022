663538ebae980c208046e3572a397fc125e3ad75
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-03 16:12:08 +0300

    2. ulesanne. Lahendus 1

---
+++ EX/ex01_hello/poem.py
@@ -0,0 +1,13 @@
+"""EX01 Poem."""
+
+# ask for a color
+color = input("What color do You prefer? ")
+# ask for objects
+objects = input("What objects do You want to see in blue? ")
+# ask for activity
+activity = input("What activity do You prefer? ")
+
+print(f"""Roses are {color},  
+{objects} are blue,  
+I love to {activity}  
+And so will you!""")
