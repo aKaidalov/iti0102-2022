b3b157378741cdd4034cb0ab5cb3ac017da8cb5e
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-07 21:49:44 +0300

    2. ulesanne. Protsess

--- EX/ex02_math/maths.py
+++ EX/ex02_math/maths.py
@@ -3,23 +3,14 @@
 
 def average(a: int, b: int, c: int, d: int):
     """Calculate the arithmetic mean of the obtained numbers."""
-    arithmetic_average = 0
-    if a:
-        arithmetic_average += a * 1
-    if b:
-        arithmetic_average += b * 2
-    if c:
-        arithmetic_average += c * 3
-    if d:
-        arithmetic_average += d * 4
-    return arithmetic_average
+    return ((a * 1) + (b * 2) + (c * 3) + (d * 4)) / 4
 
 
-# print(average(2, 2, 2, 2))
+# print(average(1, 2, 3, 4))
 
 
 def school_pressure(ects: int, weeks: int):
-    """Calculates the student's required contribution each week."""
+    """Calculate the student's required contribution each week."""
     study_hours = ects * 26  # How many hours a student needs to study
     hours_in_weeks = weeks * 7 * 24
     if hours_in_weeks > study_hours:
@@ -27,5 +18,15 @@ def school_pressure(ects: int, weeks: int):
     else:
         return -1
 
+# print("Weekly you need to spent: " + str(school_pressure(1, 0)) + " hours")
 
-print(school_pressure(6, 1))
+
+def add_fractions(a: int, b: int, c: int, d: int):
+    if (b or d) == 0:
+        return print("Can't divide by 0")
+    if b == d:
+        numerator = a + c
+        denominator = b
+    else:
+        numerator = a * d + c * b
+        denominator = b * d
