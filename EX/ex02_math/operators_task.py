a0a93db0e13982002073ff157849f124c434df46
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-07 15:27:51 +0300

    1. ulesanne. Lahendus 1

--- EX/ex02_math/operators_task.py
+++ EX/ex02_math/operators_task.py
@@ -30,3 +30,33 @@ def floor_div(x: int, y: int):
     """Divide x by y and floor the value. Use an arithmetic operator."""
     return x // y
 
+
+def exponent(x: int, y: int):
+    """Calculate x raised to the power of y."""
+    return x ** y
+
+
+def first_greater_or_equal(x: int, y: int):
+    """If x is greater or equal than y then return True. If not then return False."""
+    if x >= y:
+        return True
+    else:
+        return False
+
+
+def second_less_or_equal(x: int, y: int):
+    """If y is less or equal than x then return True. If not then return False."""
+    if y <= x:
+        return True
+    else:
+        return False
+
+
+def x_is_y(x: int, y: int):
+    """If x value is the same as y value then return True. If not then return False."""
+    if x == y:
+        return True
+    else:
+        return False
+
+
