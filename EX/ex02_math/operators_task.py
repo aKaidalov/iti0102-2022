f9e02f0c69dd51b498a26736463349ff5d04f3c9
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-07 14:57:40 +0300

    1. ulesanne. Lahendus 1

---
+++ EX/ex02_math/operators_task.py
@@ -0,0 +1,32 @@
+"""Math operators."""
+
+
+def add(x: int, y: int):
+    """Add x to y."""
+    return x + y
+
+
+def sub(x: int, y: int):
+    """Subtract y from x."""
+    return x - y
+
+
+def multiply(x: int, y: int):
+    """Multiply x by y."""
+    return x * y
+
+
+def div(x: int, y: int):
+    """Divide x by y."""
+    return x / y
+
+
+def modulus(x: int, y: int):
+    """Divide x by y and return remainder. Use an arithmetic operator."""
+    return x % y
+
+
+def floor_div(x: int, y: int):
+    """Divide x by y and floor the value. Use an arithmetic operator."""
+    return x // y
+
