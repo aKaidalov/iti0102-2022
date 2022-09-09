12c1895bb04b42c762cefbcb8fad35b35bd338d0
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-09 12:21:52 +0300

    4. ulesanne. Lahendus 0

--- EX/ex02_math/caesar.py
+++ EX/ex02_math/caesar.py
@@ -5,7 +5,7 @@ def encode(message: str, shift: int):
     for element in message:
         if shift > 26:
             shift %= 26
-        if 122 < ord(element) < 97:
+        if 122 < ord(element) < 97 and not 32:
             return False
         elif element == " ":
             print(" ", end="")
@@ -21,8 +21,8 @@ def encode(message: str, shift: int):
 
 
 if __name__ == '__main__':
-    print(encode("i like turtles", 6))  # -> o roqk zaxzrky
-    print(encode("o roqk zaxzrky", 20))  # -> i like turtles
-    print(encode("example", 1))  # -> fybnqmf
-    print(encode("don't change", 0))  # -> don't change
-    print(encode('the quick brown fox jumps over the lazy dog.', 7))  # -> aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.
+    encode("i like turtles", 6)  # -> o roqk zaxzrky
+    # print(encode("o roqk zaxzrky", 20))  # -> i like turtles
+    # print(encode("example", 1))  # -> fybnqmf
+    # print(encode("don't change", 0))  # -> don't change
+    # print(encode('the quick brown fox jumps over the lazy dog.', 7))  # -> aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.
