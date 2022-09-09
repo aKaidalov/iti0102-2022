0d48de8683bbe900e7a93c22c042da1d9f0601c4
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-09 14:24:47 +0300

    4. ulesanne. Lahendus 1

--- EX/ex02_math/caesar.py
+++ EX/ex02_math/caesar.py
@@ -2,27 +2,26 @@
 
 
 def encode(message: str, shift: int):
-    for element in message:
-        if shift > 26:
-            shift %= 26
-        if 122 < ord(element) < 97 and not 32:
-            return False
-        elif element == " ":
-            print(" ", end="")
-            continue
+    """Encode a message using a Caesar cipher."""
+    new_message: str = ""
+    if shift > 26:  # If shift = 26 than it is the same letter
+        shift %= 26  # Skips useless laps (lap = 26) and finds useful shift
+    for element in message:  # Loop checks every letter per iteration
+        if ord(element) > 122 or 97 > ord(element):  # Elements that not from a...z
+            new_message += element  # Leave item the same
         else:
-            element_num = ord(element) + shift
-            if element_num > 122:
-                element_num = 96 + (element_num - 122)
-                element = chr(element_num)
+            new_element = ord(element) + shift
+            if new_element > 122:
+                new_element = 96 + (new_element - 122)  # Searches for element in range from a...z
+                new_message += chr(new_element)  # Adds new element
             else:
-                element = chr(element_num)
-            print(element, end="")
+                new_message += chr(new_element)
+    return new_message
 
 
 if __name__ == '__main__':
-    encode("i like turtles", 6)  # -> o roqk zaxzrky
-    # print(encode("o roqk zaxzrky", 20))  # -> i like turtles
-    # print(encode("example", 1))  # -> fybnqmf
-    # print(encode("don't change", 0))  # -> don't change
-    # print(encode('the quick brown fox jumps over the lazy dog.', 7))  # -> aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.
+    print(encode("i like turtles", 6))  # -> o roqk zaxzrky
+    print(encode("o roqk zaxzrky", 20))  # -> i like turtles
+    print(encode("example", 1))  # -> fybnqmf
+    print(encode("don't change", 0))  # -> don't change
+    print(encode('the quick brown fox jumps over the lazy dog.', 7))  # -> aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.
