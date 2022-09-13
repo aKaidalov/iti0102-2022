49970393427455a0df8edec7761dd52baa001b88
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-13 16:00:09 +0300

    1. ulesanne. Lahendus 0

---
+++ EX/ex03_idcode/idcode.py
@@ -0,0 +1,38 @@
+"""EX03 ID code."""
+
+
+def find_id_code(text: str) -> str:
+    """
+    Find ID-code from given text.
+
+    Given string may include any number of numbers, characters and other symbols mixed together.
+    The numbers of ID-code may be between other symbols - they must be found and concatenated.
+    ID-code contains of exactly 11 numbers. If there are not enough numbers, return 'Not enough numbers!',
+    if there are too many numbers, return 'Too many numbers!' If ID-code can be found, return that code.
+    You don't have to validate the ID-code here. If it has 11 numbers, then it is enough for now.
+
+    :param text: string
+    :return: string
+    """
+    # "0123456789" = 48 - 57 ASCII
+    new_text: str = ""
+    counter = 0
+    for element in text:
+        new_element = ord(element)
+        if 47 < new_element and new_element < 58:
+            new_text += element
+            counter += 1
+    if counter > 11:
+        return "Too many numbers!"
+    elif counter < 11:
+        return "Not enough numbers!"
+    else:
+        return new_text
+
+
+if __name__ == '__main__':
+    print("\nFind ID code:")
+    print(find_id_code(""))  # -> "Not enough numbers!"
+    print(find_id_code("123456789123456789"))  # -> "Too many numbers!"
+    print(find_id_code("ID code is: 49403136526"))  # -> "49403136526"
+    print(find_id_code("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
