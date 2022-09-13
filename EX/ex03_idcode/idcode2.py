2c5a38d9a6b28317d89febefb3ad8ff538b6b185
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-13 20:36:41 +0300

    2. ulesanne. Lahendus 1

--- EX/ex03_idcode/idcode2.py
+++ EX/ex03_idcode/idcode2.py
@@ -3,21 +3,40 @@
 
 def is_valid_gender_number(gender_number: int):
     """Check if given value is correct for gender number in ID code."""
+    if gender_number < 1 or gender_number > 6:
+        return False
+    else:
+        return True
+
+
+def get_gender(gender_number: int):
+    """Determines if gender is male or female."""
+    modulo = gender_number % 2
+    if modulo != 0:
+        return "male"
+    else:
+        return "female"
 
 
 def is_valid_year_number(year_number: int) -> bool:
     """Check if given value is correct for year number in ID code."""
-    # Write your code here
+    if -1 < year_number < 100:
+        return True
+    return False
 
 
 def is_valid_month_number(month_number: int) -> bool:
     """Check if given value is correct for month number in ID code."""
-    # Write your code here
+    if 0 < month_number < 13:
+        return True
+    return False
 
 
 def is_valid_birth_number(birth_number: int) -> bool:
     """Check if given value is correct for birth number in ID code."""
-    # Write your code here
+    if 0 < birth_number < 1000:
+        return True
+    return False
 
 
 if __name__ == '__main__':
