7d46775b5a8c7ac97a67604aa071e32a14b40966
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-09-16 14:38:15 +0300

    1.2.3.4. ulesanne. Lahendus 11

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -20,6 +20,7 @@ def find_id_code(text: str) -> str:
         return new_text
 
 
+# Sledujushuju funktsiju mozno b6lob6 cokratit', isspolzovav pervuju
 def the_first_control_number_algorithm(text: str) -> str:
     """Check if given value is correct for control number in ID code only with the first algorithm."""
     new_text: str = ""
@@ -138,19 +139,18 @@ def get_birth_place(birth_number: int) -> str:
 
 def is_valid_control_number(id_code: str) -> bool:
     """Check if given value is correct for control number in ID code."""
-    new_id_code = find_id_code(id_code)
     sum_of_seven2, sum_of_ten_numbers2, sum_of_three2 = 0, 0, 0
-    if the_first_control_number_algorithm(new_id_code) == "Incorrect ID code!":
+    if the_first_control_number_algorithm(id_code) == "Incorrect ID code!":
         return False
-    elif the_first_control_number_algorithm(new_id_code) == "Needs the second algorithm!":
+    elif the_first_control_number_algorithm(id_code) == "Needs the second algorithm!":
         for i in range(7):
-            sum_of_seven2 += int(new_id_code[i + 3]) * (i + 3)  # multiplies with its corresponding digit (until 7 digits)
+            sum_of_seven2 += int(id_code[i + 3]) * (i + 3)  # multiplies with its corresponding digit (until 7 digits)
             if i == 9:
                 for iteration in range(7, 10):
-                    sum_of_three2 += int(new_id_code[iteration - 6]) * (iteration - 6)  # multiplies last 3 digits
+                    sum_of_three2 += int(id_code[iteration - 6]) * (iteration - 6)  # multiplies last 3 digits
         sum_of_ten_numbers2 = sum_of_seven2 + sum_of_three2
         remainder2 = sum_of_ten_numbers2 % 11
-        if remainder2 < 10 and remainder2 == int(new_id_code[10]):
+        if remainder2 < 10 and remainder2 == int(id_code[10]):
             return True
         else:
             return False
@@ -159,78 +159,78 @@ def is_valid_control_number(id_code: str) -> bool:
 
 
 # Mb gde to zdes' oshibka
-# def is_valid_day_number(gender_number: int, year_number: int, month_number: int, day_number: int) -> bool:
-#     """Check if given value is correct for day number in ID code."""
-#     if is_valid_gender_number(gender_number) and is_valid_year_number(year_number) and is_valid_month_number(month_number):
-#         # Previous line checks if all the numbers are correct.
-#         days_in_month_31 = [1, 3, 5, 7, 8, 10, 12]
-#         days_in_month_30 = [4, 6, 9, 11]
-#         for element in range(len(days_in_month_31)):
-#             if month_number == days_in_month_31[element] and 0 < day_number < 32:
-#                 return True
-#         for element in range(len(days_in_month_30)):
-#             if month_number == days_in_month_30[element] and 0 < day_number < 31:
-#                 return True
-#         year_in_4_numbers = get_full_year(gender_number, year_number)
-#         if is_leap_year(year_in_4_numbers) and 0 < day_number < 30:
-#             return True
-#         else:
-#             if 0 < day_number < 29:
-#                 return True
-#             return False
-#     else:
-#         return False
-#
-#
-# def is_id_valid(id_code: str) -> bool:
-#     """Check if given ID code is valid and return the result (True or False)."""
-#     if is_valid_control_number(id_code):
-#         year_number, month_number, day_number, gender_number = "", "", "", 0
-#         for index in range(len(id_code)):
-#             if index == 0:
-#                 gender_number = int(id_code[index])
-#             if index == 1 or index == 2:
-#                 year_number += id_code[index]
-#             if index == 3 or index == 4:
-#                 month_number += id_code[index]
-#             if index == 5 or index == 6:
-#                 day_number += id_code[index]
-#         year_number = int(year_number)
-#         month_number = int(month_number)
-#         day_number = int(day_number)
-#         if is_valid_day_number(gender_number, year_number, month_number, day_number):
-#             return True
-#         else:
-#             return False
-#
-#
-# def get_data_from_id(id_code: str) -> str:
-#     """Get possible information about the person."""
-#     if is_id_valid(id_code):
-#         year_number, month_number, day_number, birth_number, gender_number, gender, location = "", "", "", "", 0, "", ""
-#         for index in range(len(id_code)):
-#             if index == 0:
-#                 gender_number = int(id_code[index])
-#             if index == 1 or index == 2:
-#                 year_number += id_code[index]
-#             if index == 3 or index == 4:
-#                 month_number += id_code[index]
-#             if index == 5 or index == 6:
-#                 day_number += id_code[index]
-#             if index == 7 or index == 8 or index == 9:
-#                 birth_number += id_code[index]
-#
-#         gender = get_gender(gender_number)
-#         year_number = int(year_number)
-#         ful_year = str(get_full_year(gender_number, year_number))
-#         birth_number = int(birth_number)
-#
-#         if is_valid_birth_number(birth_number):
-#             location = get_birth_place(birth_number)
-#
-#         return f"This is a {gender} born on {day_number}.{month_number}.{ful_year} in {location}"
-#     else:
-#         return "Given invalid ID code!"
+def is_valid_day_number(gender_number: int, year_number: int, month_number: int, day_number: int) -> bool:
+    """Check if given value is correct for day number in ID code."""
+    if is_valid_gender_number(gender_number) and is_valid_year_number(year_number) and is_valid_month_number(month_number):
+        # Previous line checks if all the numbers are correct.
+        days_in_month_31 = [1, 3, 5, 7, 8, 10, 12]
+        days_in_month_30 = [4, 6, 9, 11]
+        for element in range(len(days_in_month_31)):
+            if month_number == days_in_month_31[element] and 0 < day_number < 32:
+                return True
+            elif element == 6:
+                for index in range(len(days_in_month_30)):
+                    if month_number == days_in_month_30[index] and 0 < day_number < 31:
+                        return True
+        year_in_4_numbers = get_full_year(gender_number, year_number)
+        if is_leap_year(year_in_4_numbers) and 0 < day_number < 30:
+            return True
+        else:
+            if 0 < day_number < 29:
+                return True
+            return False
+    return False
+
+
+def is_id_valid(id_code: str) -> bool:
+    """Check if given ID code is valid and return the result (True or False)."""
+    if is_valid_control_number(id_code):
+        year_number, month_number, day_number, gender_number = "", "", "", 0
+        for index in range(len(id_code)):
+            if index == 0:
+                gender_number = int(id_code[index])
+            if index == 1 or index == 2:
+                year_number += id_code[index]
+            if index == 3 or index == 4:
+                month_number += id_code[index]
+            if index == 5 or index == 6:
+                day_number += id_code[index]
+        year_number = int(year_number)
+        month_number = int(month_number)
+        day_number = int(day_number)
+        if is_valid_day_number(gender_number, year_number, month_number, day_number):
+            return True
+        else:
+            return False
+
+
+def get_data_from_id(id_code: str) -> str:
+    """Get possible information about the person."""
+    if is_id_valid(id_code):
+        year_number, month_number, day_number, birth_number, gender_number, gender, location = "", "", "", "", 0, "", ""
+        for index in range(len(id_code)):
+            if index == 0:
+                gender_number = int(id_code[index])
+            if index == 1 or index == 2:
+                year_number += id_code[index]
+            if index == 3 or index == 4:
+                month_number += id_code[index]
+            if index == 5 or index == 6:
+                day_number += id_code[index]
+            if index == 7 or index == 8 or index == 9:
+                birth_number += id_code[index]
+
+        gender = get_gender(gender_number)
+        year_number = int(year_number)
+        ful_year = str(get_full_year(gender_number, year_number))
+        birth_number = int(birth_number)
+
+        if is_valid_birth_number(birth_number):
+            location = get_birth_place(birth_number)
+
+        return f"This is a {gender} born on {day_number}.{month_number}.{ful_year} in {location}"
+    else:
+        return "Given invalid ID code!"
 
 
 if __name__ == '__main__':
@@ -241,28 +241,28 @@ if __name__ == '__main__':
     # the second algorithm!"(Ne proverjaet ljuboi string tk vernut'
     # pervaja f moshet libo needs... libo otvet, libo oshibku)
 
-    # print("\nDay number:")
-    # print(is_valid_day_number(4, 5, 12, 25))  # -> True
-    # print(is_valid_day_number(3, 10, 8, 32))  # -> False
-    # print("\nFebruary check:")
-    # print(
-    #     is_valid_day_number(4, 96, 2, 30))  # -> False (February cannot contain more than 29 days in any circumstances)
-    # print(is_valid_day_number(4, 99, 2, 29))  # -> False (February contains 29 days only during leap year)
-    # print(is_valid_day_number(4, 8, 2, 29))  # -> True
-    # print("\nMonth contains 30 or 31 days check:")
-    # print(is_valid_day_number(4, 22, 4, 31))  # -> False (April contains max 30 days)
-    # print(is_valid_day_number(4, 18, 10, 31))  # -> True
-    # print(is_valid_day_number(4, 15, 9, 31))  # -> False (September contains max 30 days)
-    #
-    # print("\nOverall ID check::")
-    # print(is_id_valid("49808270244"))  # -> True
-    # print(is_id_valid("12345678901"))  # -> False
-    #
-    # print("\nFull message:")
-    # print(get_data_from_id("49808270244"))  # -> "This is a female born on 27.08.1998 in Tallinn."
-    # print(get_data_from_id("60109200187"))  # -> "Given invalid ID code!"
-    # print(get_data_from_id("50204073724"))
-    #
-    # print("\nTest now your own ID code:")
-    # personal_id = input()  # type your own id in command prompt
-    # print(is_id_valid(personal_id))  # -> True
+    print("\nDay number:")
+    print(is_valid_day_number(4, 5, 12, 25))  # -> True
+    print(is_valid_day_number(3, 10, 8, 32))  # -> False
+    print("\nFebruary check:")
+    print(
+        is_valid_day_number(4, 96, 2, 30))  # -> False (February cannot contain more than 29 days in any circumstances)
+    print(is_valid_day_number(4, 99, 2, 29))  # -> False (February contains 29 days only during leap year)
+    print(is_valid_day_number(4, 8, 2, 29))  # -> True
+    print("\nMonth contains 30 or 31 days check:")
+    print(is_valid_day_number(4, 22, 4, 31))  # -> False (April contains max 30 days)
+    print(is_valid_day_number(4, 18, 10, 31))  # -> True
+    print(is_valid_day_number(4, 15, 9, 31))  # -> False (September contains max 30 days)
+
+    print("\nOverall ID check::")
+    print(is_id_valid("49808270244"))  # -> True
+    print(is_id_valid("12345678901"))  # -> False
+
+    print("\nFull message:")
+    print(get_data_from_id("49808270244"))  # -> "This is a female born on 27.08.1998 in Tallinn."
+    print(get_data_from_id("60109200187"))  # -> "Given invalid ID code!"
+    print(get_data_from_id("50204073724"))
+
+    print("\nTest now your own ID code:")
+    personal_id = input()  # type your own id in command prompt
+    print(is_id_valid(personal_id))  # -> True
