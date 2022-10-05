942e5ced1c9577616e7e6fdcb870c6fdd3799ec9
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-10-06 00:56:51 +0300

    1. ulesanne 7

--- EX/ex06_regex/regex.py
+++ EX/ex06_regex/regex.py
@@ -72,7 +72,7 @@ def find_words_from_sentence(sentence: str) -> list:
     :param sentence: given sentence to find words from
     :return: list of words found in given sentence
     """
-    return re.findall(r"[A-ZÕÜÄÖa-zõüäö][^\W]*", sentence)
+    return re.findall(r"[A-ZÕÜÄÖa-zõüäö\d*]\w*", sentence)
 
 
 def find_words_from_sentences_only(text: str) -> list:
@@ -152,7 +152,7 @@ if __name__ == '__main__':
         'See on esimene - lause. See on ä teine lause! see ei ole lause. Aga kas see on? jah, oli.'))  # ['See on esimene - lause.', 'See on ä teine lause!', 'Aga kas see on?']
     print(find_words_from_sentence("Super lause ää, sorry."))  # ['Super', 'lause', 'ää', 'sorry']
     print(find_words_from_sentences_only(
-        'See on esimene - ä lause. See, on teine: lause! see ei ole lause. Aga kas see on? jah, oli.'))  # ['See', 'on', 'esimene', 'ä', 'lause', 'See', 'on', 'teine', 'lause', 'Aga', 'kas', 'see', 'on']
+        'See on esimene - ä lause. See, on teine: lause! see ei ole lause. Aga kas see 1994 on? jah, oli.'))  # ['See', 'on', 'esimene', 'ä', 'lause', 'See', 'on', 'teine', 'lause', 'Aga', 'kas', 'see', 'on']
     print(find_years("1998sef672387fh3f87fh83777f777f7777f73wfj893w8938434343"))  # [1998, 7777]
     print(find_phone_numbers(
         "+372 56887364  +37256887364  +33359835647  56887364"))  # {'+372': ['56887364', '56887364'], '+333': ['59835647'], '': ['56887364']}
