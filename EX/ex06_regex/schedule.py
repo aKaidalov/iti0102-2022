a2eec805428d545b146943b0c9673f3257904fd6
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-10-09 18:47:07 +0300

    3. ulesanne demo

--- EX/ex06_regex/schedule.py
+++ EX/ex06_regex/schedule.py
@@ -2,26 +2,6 @@
 import re
 
 
-# def create_table(...):
-#     """Create table."""
-#
-# def get_table_sizes(...):
-#     """Get the maximum sizes for table."""
-#
-# def get_hours_sorted(input_string: str):
-#     """Sort hours in 24 format."""
-#     dic = {}
-#     for match in re.finditer(r"(?<=\s)(\d{1,2})\D(\d{1,2})\s+([A-Za-z]+)", input_string):
-#         hours, minutes = int(match.group(1)), int(match.group(2))
-#         if 0 <= hours < 24 and 0 <= minutes < 60:
-#             hour = int(match.group(1))
-#             if hour not in dic:
-#                 dic[hour] = [int(match.group(2))]
-#             else:
-#                 dic[hour].append(int(match.group(2)))
-#     return sorted(dic.items())
-
-
 def get_formatted_time(input_string: str):
     """Format 24 hour time to the 12 hour time."""
     dic = {}
@@ -44,42 +24,34 @@ def get_formatted_time(input_string: str):
 
 
 def sorted_am_and_pm(input_string: str):
-    """Sort times."""
+    """Sort times by AM and PM."""
     dic = get_formatted_time(input_string)
     keys = dic.keys()
-    new_dic = {}        # for sorting dic
+    am_list = []
+    pm_list = []
     for key in keys:
-        split = key.split(":")
-        hour = int(split[0])
-        double_split = split[1].split(" ")      # split minutes and AM/PM
-        minutes = int(double_split[0])
-        midday = double_split[1]
-        time = [hour, minutes]
-        if midday not in new_dic:
-            new_dic[midday] = [time]
+        if "AM" in key:
+            am_list.append(key)
         else:
-            new_dic[midday].append(time)
-    return new_dic
+            pm_list.append(key)
+    return [am_list, pm_list]
 
 
-def sorted_hours(input_string: str):
-    """Sort times."""
-    dic = sorted_am_and_pm(input_string)
-    keys = sorted(dic.keys())
-    new_dic = {}
-    hours_in_dic = {}
-    for key in keys:        # to get AM and then PM
-        for element in dic[key]:        # element = time [11, 1]
-            if element[0] not in hours_in_dic:
-                hours_in_dic[element[0]] = [element[1]]
-            else:
-                hours_in_dic[element[0]].append(element[1])
-        if key not in new_dic:
-            new_dic[key] = [sorted(hours_in_dic.items())]
-        else:
-            new_dic[key].append(sorted(hours_in_dic.items()))
-
-    return new_dic
+def sorted_am_or_pm_hours(midday_list: list):
+    """Sort by hours."""
+    hours = []
+    new_am_or_pm_list = []
+    for element in midday_list:
+        el = element.split(":")
+        if int(el[0]) not in hours:     # convert el[0] in int because hours list consists of int
+            hours.append(int(el[0]))
+    hours_were_sorted = sorted(hours)
+    for h in hours_were_sorted:
+        for e in midday_list:
+            control = e.split(":")
+            if h == int(control[0]) and control[0] not in new_am_or_pm_list:
+                new_am_or_pm_list.append(e)
+    return new_am_or_pm_list
 
 
 def create_schedule_file(input_filename: str, output_filename: str) -> None:
@@ -87,11 +59,16 @@ def create_schedule_file(input_filename: str, output_filename: str) -> None:
     pass
 
 
-def create_schedule_string(input_string: str) -> str:
+def create_schedule_string(input_string: str):      # -> str: Ubral dlja proverki
     """Create schedule string from the given input string."""
-    pass
+    am_and_pm_lists = sorted_am_and_pm(input_string)
+    am_list = am_and_pm_lists[0]
+    pm_list = am_and_pm_lists[1]
+    am_sorted_hours = sorted_am_or_pm_hours(am_list)
+    pm_sorted_hours = sorted_am_or_pm_hours(pm_list)
+    return am_sorted_hours, pm_sorted_hours
 
 
 if __name__ == '__main__':
-    print(sorted_hours("wat 13:00 teine tekst 11:0 23-59 canuseminustherege pikktekst 08:01 Lorem  21:59 nopoint 18:19 Donec 18.1 ds"))
+    print(create_schedule_string("wat 13:00 wat 10:00 teine tekst 11:0 23-59 canuseminustherege pikktekst 08:01 Lorem  21:59 nopoint 18:19 Donec 18.1 ds 09:01 Lorem 0:0 Lorem 8:4 Lorem"))
     create_schedule_file("schedule_input.txt", "schedule_output.txt")
