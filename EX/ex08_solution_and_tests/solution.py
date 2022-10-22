e8b2726cc1fc741b83c5f1e9cbb69b4589049bee
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-10-22 15:48:35 +0300

    1. ulesanne

---
+++ EX/ex08_solution_and_tests/solution.py
@@ -0,0 +1,40 @@
+def students_study(time: int, coffee_needed: bool) -> bool:
+    """
+    Return True if students study in given circumstances.
+
+    (19, False) -> True
+    (1, True) -> False.
+    """
+    if (5 <= time <= 17 and coffee_needed is True) or (18 <= time <= 24 and coffee_needed is False):
+        return True
+    return False
+
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """
+    Return Lottery victory result 10, 5, 1, or 0 according to input values.
+
+    (5, 5, 5) -> 10
+    (2, 2, 1) -> 0
+    (2, 3, 1) -> 1
+    """
+    if a == b == c == 5:
+        return 10
+    if a == b == c:
+        return 5
+    if a != b != c:
+        return 1
+    return 0
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """
+    Return number of small fruit baskets if it's possible to finish the order, otherwise return -1.
+
+    (4, 1, 9) -> 4
+    (3, 1, 10) -> -1
+    """
+    if small_baskets + big_baskets * 5 == ordered_amount:
+        return small_baskets
+    return -1
