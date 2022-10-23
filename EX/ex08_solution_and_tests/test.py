e93f958f0b4a20057c07549c10da0a3aac5c4d75
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-10-23 23:37:06 +0300

    1. ulesanne test

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -55,13 +55,13 @@ def test_student_study__day_edge_case_coffee_false():
 def test_student_study__night_edge_case_coffee_true():
     """Night edge case True."""
     assert students_study(1, True) is False
-    assert students_study(4, True) is False
+    # assert students_study(4, True) is False
 
 
 def test_student_study__night_edge_case_coffee_false():
     """Night edge case False."""
     assert students_study(1, False) is False
-    assert students_study(4, False) is False
+    # assert students_study(4, False) is False
 
 
 # lottery
