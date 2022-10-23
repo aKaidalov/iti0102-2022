0515f467ae207f63946e715860c1fe2396ca84cf
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-10-23 22:07:08 +0300

    1. ulesanne test

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -5,7 +5,7 @@ from solution import students_study
 
 def test_students_study__evening_not_coffee_needed():
     """In the evening it doesn't matter weather we have coffee."""
-    assert students_study(20, True) is False
+    assert students_study(20, True) is True
     assert students_study(20, False) is True
 
 
