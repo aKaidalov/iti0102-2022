8daa7a976cac398230da67326745d7544b857d4c
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-10-24 09:23:46 +0300

    1. ulesanne test

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -135,4 +135,4 @@ def test_fruit_order__zero_amount_others_not_zero():
 
 def test_fruit_order__only_big_exact_match():
     """Amount of big baskets is equal to order amount."""
-    assert fruit_order(3, 3, 15) == 0
+    assert fruit_order(0, 2, 10) == 0
