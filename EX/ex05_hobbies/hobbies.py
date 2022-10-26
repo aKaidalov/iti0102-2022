1414f1e86b066375cd567d64ae7d1f60edcdd13b
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-10-26 22:46:30 +0300

    1. ulesanne test

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -249,11 +249,16 @@ def find_two_people_with_most_common_hobbies(data: str) -> tuple:
 
 def sorted_sorted ():
     people = {3: "Jim", 2: "Jack", 4: "Jane", 1: "Jill"}
-    print(dict(sorted(people.items())))
+    print(sorted(people.items()))
 
 sorted_sorted()
-# if __name__ == '__main__':
+
+if __name__ == '__main__':
     # sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
+    # sample_data = """Jack:crafting\nPeter:hiking\nCooper:yoga\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nPeter:driving\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
+    # sample_data = """Jack:crafting\n"""
+    # print(create_dictionary(sample_data))
+    print(create_dictionary("""Jack:crafting\nPeter:hiking\nPeter:driving\nJack:fitness\nPeter:cooking"""))
     # dic = create_dictionary(sample_data)
     # print("shopping" in dic["Wendy"])  # -> True
     # print("fitness" in dic["Sophie"])  # -> False
