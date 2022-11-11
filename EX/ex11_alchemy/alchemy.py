4af67b0b0cc59956b5bdde5ecff7b5b266c389e3
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-11-11 21:58:57 +0200

    ex11 2.u

--- EX/ex11_alchemy/alchemy.py
+++ EX/ex11_alchemy/alchemy.py
@@ -227,6 +227,7 @@ class Cauldron(AlchemicalStorage):
                         break
                 if check:
                     super().add(element)
+        raise TypeError("Only alchemical elements are allowed")
 
 
 if __name__ == '__main__':
