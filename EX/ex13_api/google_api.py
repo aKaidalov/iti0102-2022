b294ac3b31ad006d36227f09533507ffe04f4007
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-11-29 14:49:43 +0200

    ex13 2.u

--- EX/ex13_api/google_api.py
+++ EX/ex13_api/google_api.py
@@ -113,7 +113,7 @@ def get_links_from_playlist(link: str, developer_key: str) -> list:
     res = []
     for elements in response["items"]:
         res.append(f"https://youtube.com/watch?v={elements['id']}")
-    print(res)
+    return res
 
 
 if __name__ == '__main__':
