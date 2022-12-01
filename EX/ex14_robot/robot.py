81f6aaf4a99f531988716062833c87e93d9082f1
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-12-01 16:46:54 +0200

    ex14 3.u

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -70,12 +70,12 @@ def follow_the_line(robot: FollowerBot):
         else:
             color3 = 0
             while color3 != 1024:
-                robot.set_right_wheel_speed(-50)
+                robot.set_right_wheel_speed(-60)
                 robot.sleep(0.01)
                 robot.set_right_wheel_speed(0)
                 color3 = robot.get_third_line_sensor_from_right()
                 if color3 == 1024:
-                    robot.set_right_wheel_speed(50)
+                    robot.set_right_wheel_speed(60)
                     robot.sleep(0.01)
                     robot.set_right_wheel_speed(0)
             if robot.get_third_line_sensor_from_right() and robot.get_third_line_sensor_from_left():
