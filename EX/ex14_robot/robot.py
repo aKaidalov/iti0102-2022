ed69567137da4ce2818097b4b46c2b38bf8c0847
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-12-01 16:04:28 +0200

    ex14 3.u

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -54,10 +54,16 @@ def follow_the_line(robot: FollowerBot):
 
     # follows the black line with "u" pattern
     color2 = robot.get_left_line_sensor()
+    turn = 0
     while color2 == 0:
-        if robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0:
+        if robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0 and turn % 2 == 0:
             robot.set_left_wheel_speed(100)
             robot.set_right_wheel_speed(50)
+            robot.sleep(0.01)
+            robot.set_wheels_speed(0)
+        elif robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0 and turn % 2 != 0:
+            robot.set_left_wheel_speed(50)
+            robot.set_right_wheel_speed(100)
             robot.sleep(0.01)
             robot.set_wheels_speed(0)
         elif robot.get_left_line_sensor() != 0 and robot.get_right_line_sensor() == 0:
@@ -70,6 +76,8 @@ def follow_the_line(robot: FollowerBot):
             robot.set_right_wheel_speed(0)
         else:
             color2 = 1
+
+        turn += 1
 
     robot.done()
 
