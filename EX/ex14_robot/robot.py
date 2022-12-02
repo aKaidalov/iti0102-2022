58d8fb0c102cc8ca0cc18acc4d77bd0841e99199
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-12-02 10:36:37 +0200

    ex14 3.u

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -70,12 +70,12 @@ def follow_the_line(robot: FollowerBot):
             # for "п" pattern
             for j in range(10000):
                 robot.set_right_wheel_speed(-50)
-                robot.sleep(0.1)
+                robot.sleep(0.01)
                 robot.set_right_wheel_speed(0)
                 color3 = robot.get_third_line_sensor_from_right()
                 if color3 == 1024:
                     robot.set_right_wheel_speed(50)
-                    robot.sleep(0.1)
+                    robot.sleep(0.01)
                     robot.set_right_wheel_speed(0)
                     break
             if robot.get_third_line_sensor_from_right() and robot.get_third_line_sensor_from_left():
@@ -91,3 +91,7 @@ def the_true_follower(robot: FollowerBot):
     :param FollowerBot robot: instance of the robot that you need to make move
     """
     pass
+
+
+if __name__ == "__main__":
+    follow_the_line(robot=FollowerBot())
