7863d5716199402b7c23601de789ec3b7cb6d4dd
Aleksandr Kaidalov <alkaid@ttu.ee>
2022-12-02 15:27:32 +0200

    ex14 3.u

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -134,6 +134,52 @@ def drive_to_line_for_last_function(robot: FollowerBot):
         color = robot.get_left_line_sensor()
 
 
+def follow_the_line_for_last_function(robot: FollowerBot):
+    """
+    Create a FollowerBot that will follow a black line until the end of that line.
+
+    The robot's starting position will be just short of the start point of the line.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
+    # drives to a line on a white surface
+    robot.set_wheels_speed(30)
+    robot.sleep(1)
+
+    turn = 0
+    # follows the black line with "u" pattern
+    for i in range(5000):
+        left_line_sensor = robot.get_left_line_sensor()
+        right_line_sensor = robot.get_right_line_sensor()
+
+        if left_line_sensor == 0 and right_line_sensor == 0:
+            robot.set_wheels_speed(100)
+            robot.sleep(0.03)
+            robot.set_wheels_speed(0)
+        elif left_line_sensor != 0 and right_line_sensor == 0:
+            robot.set_left_wheel_speed(50)
+            robot.sleep(0.01)
+            robot.set_left_wheel_speed(0)
+        elif left_line_sensor == 0 and right_line_sensor != 0:
+            robot.set_right_wheel_speed(50)
+            robot.sleep(0.01)
+            robot.set_right_wheel_speed(0)
+        else:
+            # hardcode for "п" pattern with 2 turns. turn == 6 because robot does 2 turns in 6 rotations.
+            if turn == 8:
+                break
+            turn += 1
+            # for "п" pattern
+            if not robot.get_third_line_sensor_from_left() and robot.get_third_line_sensor_from_right() > 0:
+                turn_left(robot)
+            elif robot.get_third_line_sensor_from_left() > 0 and not robot.get_third_line_sensor_from_right():
+                turn_right(robot)
+            elif robot.get_second_line_sensor_from_left() and robot.get_second_line_sensor_from_right():
+                break
+
+    robot.done()
+
+
 def the_true_follower(robot: FollowerBot):
     """
     Create a FollowerBot that will follow the black line on the track and make it ignore all possible distractions.
@@ -141,7 +187,7 @@ def the_true_follower(robot: FollowerBot):
     :param FollowerBot robot: instance of the robot that you need to make move
     """
     drive_to_line_for_last_function(robot)
-    follow_the_line(robot)
+    follow_the_line_for_last_function(robot)
 
 
 if __name__ == "__main__":
