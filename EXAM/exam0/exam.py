2956fcec5e298762b8bb806c39253d06c724f33f
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-01-06 19:14:36 +0200

    exam0 1-2

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -1,6 +1,7 @@
 """Exam 0."""
 import math
 
+
 def find_capital_letters(string1: str):
     """Find all capital letters."""
     result = ""
@@ -13,11 +14,12 @@ def find_capital_letters(string1: str):
 def close_far(a: int, b: int, c: int):
     """Return true if one number is near and other is far."""
     if abs(a - b) <= 1:
-        if abs(a - c) >= 2 or abs(b - c) >= 2:
+        if abs(a - c) >= 2 and abs(b - c) >= 2:
             return True
     elif abs(a - c) <= 1:
-        if abs(a - b) >= 2 or abs(c - b) >= 2:
+        if abs(a - b) >= 2 and abs(c - b) >= 2:
             return True
+    return False
 
 
 if __name__ == '__main__':
