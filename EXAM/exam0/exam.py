1ee1a62fafd4995eca9b9618bbca45de2016ac60
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-01-06 19:22:56 +0200

    exam0 1-2

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -1,5 +1,5 @@
 """Exam 0."""
-import math
+from typing import Optional
 
 
 def find_capital_letters(string1: str):
@@ -12,15 +12,26 @@ def find_capital_letters(string1: str):
 
 
 def close_far(a: int, b: int, c: int):
-    """Return true if one number is near and other is far."""
-    if abs(a - b) <= 1:
-        if abs(a - c) >= 2 and abs(b - c) >= 2:
-            return True
-    elif abs(a - c) <= 1:
-        if abs(a - b) >= 2 and abs(c - b) >= 2:
-            return True
-    return False
+    """
+    Return if one value is "close" and other is "far".
+
+    #2
+
+    Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1),
+    while the other is "far", differing from both other values by 2 or more.
+
+    close_far(1, 2, 10) => True
+    close_far(1, 2, 3) => False
+    close_far(4, 1, 3) => True
+    """
+    if abs(a - b) <= 1 and abs(a - c) >= 2 and abs(b - c) >= 2:
+        return True
+    elif abs(a - c) <= 1 and abs(a - b) >= 2 and abs(c - b) >= 2:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
     print(find_capital_letters("SasSHa"))
+    print(close_far(1, 2, 3))
